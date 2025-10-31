8f63d0c42b73d22b6379f0d5c07f7eead7174bd8
markjennings <mark.jennings@ca.vu>
2025-10-31 14:03:16 +0000

    Remove `useVue` example and route from navigation and project files
    - Deleted `useVue.vue` and related implementation.
    - Removed corresponding navigation entry from `site-navigation.vue`.

--- src/components/site-navigation.vue
+++ src/components/site-navigation.vue
@@ -10,9 +10,6 @@
     <li>
       <router-link :to="{ name: 'vee' }">VeeValidate</router-link>
     </li>
-    <li>
-      <router-link :to="{ name: 'useVue' }">useVue</router-link>
-    </li>
     <li><a href="https://vestjs.dev/">Vest</a></li>
     <li><a href="https://github.com/jquense/yup">Yup</a></li>
     <li><a href="https://zod.dev/">Zod</a></li>
